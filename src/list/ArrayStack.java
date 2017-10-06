e9a5228b015b594f98152ecb9729199705b05357
bianguangkuo <bgk083@126.com>
2017-10-06 22:07:58 +0800

    add stack

---
+++ src/list/ArrayStack.java
@@ -0,0 +1,68 @@
+package list;
+
+/**
+ * Created by Victor on 2017/10/6.
+ */
+public class ArrayStack {
+    private int top;
+    private int[] stack;
+    private int capacity;
+
+    public ArrayStack(int capacity) {
+        this.capacity = capacity;
+        stack = new int[this.capacity];
+        top = -1;
+    }
+
+    public void push(int data) {
+        if (top < capacity - 1) {
+            stack[++top] = data;
+        } else {
+            capacity = capacity * 2;
+            int[] stack2 = new int[capacity];
+            System.arraycopy(stack, 0, stack2, 0, stack.length);
+            stack = stack2;
+            stack[++top] = data;
+        }
+    }
+
+    public int pop() {
+        if (top == -1) {
+            throw new IndexOutOfBoundsException("underflow");
+        } else {
+            int data = stack[top];
+            top--;
+            return data;
+        }
+    }
+
+    public int top() {
+        if (top == -1) {
+            throw new IndexOutOfBoundsException("underflow");
+        } else {
+            return stack[top];
+        }
+    }
+
+    public boolean isEmpty() {
+        if (top == -1) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    public static void main(String[] args) {
+        ArrayStack stack = new ArrayStack(8);
+        stack.push(3);
+        stack.push(5);
+        stack.push(7);
+        stack.push(6);
+        stack.push(7);
+        stack.push(9);
+        System.out.println(stack.top());
+        while (!stack.isEmpty()) {
+            System.out.println(stack.pop());
+        }
+    }
+}
---
+++ src/list/LinkedListStack.java
@@ -0,0 +1,70 @@
+package list;
+
+/**
+ * Created by Victor on 2017/10/6.
+ */
+public class LinkedListStack {
+    private Node front;
+
+    public LinkedListStack() {
+        front = new Node();
+    }
+
+    public void push(int data) {
+        Node p = new Node(data);
+        p.next = front.next;
+        front.next = p;
+    }
+
+    public int pop() {
+        Node p = front.next;
+        if (p != null) {
+            front.next = p.next;
+            return p.data;
+        } else {
+            throw new IndexOutOfBoundsException("underflow");
+        }
+    }
+
+    public int top() {
+        Node p = front.next;
+        if (p != null) {
+            return p.data;
+        } else {
+            throw new IndexOutOfBoundsException("underflow");
+        }
+    }
+
+    public boolean isEmpty() {
+        return front.next == null;
+    }
+
+    public static void main(String[] args) {
+        LinkedListStack stack = new LinkedListStack();
+        stack.push(3);
+        stack.push(5);
+        stack.push(7);
+        stack.push(6);
+        stack.push(7);
+        stack.push(9);
+        System.out.println(stack.top());
+
+        while (!stack.isEmpty()) {
+            System.out.println(stack.pop());
+        }
+    }
+
+
+    private static class Node {
+        int data;
+        Node next;
+
+        public Node() {
+
+        }
+
+        public Node(int data) {
+            this.data = data;
+        }
+    }
+}
