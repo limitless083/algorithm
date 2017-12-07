451fa31b3f3b37ff16e70b190354391f864c732f
bianguangkuo <bgk083@126.com>
2017-12-07 13:16:52 +0800

    add cycle print

--- src/graph/CycleInUndirectedGraph.java
+++ src/graph/CycleInUndirectedGraph.java
@@ -1,6 +1,8 @@
 package graph;
 
+import java.util.ArrayDeque;
 import java.util.ArrayList;
+import java.util.Deque;
 import java.util.List;
 
 /**
@@ -10,12 +12,15 @@ public class CycleInUndirectedGraph {
     private int V; // No. of vertices
     private List<Integer>[] adj;
     private boolean[] visited;
-    private boolean hasCycle;
+//    private boolean hasCycle;
+    private int[] edgeTo;
+    private Deque<Integer> cycle;
 
     private CycleInUndirectedGraph(int V) {
         this.V = V;
         adj = new ArrayList[V]; // 采用邻接表存储
         visited = new boolean[V];
+        edgeTo = new int[V];
         for (int i = 0; i < V; i++) {
             adj[i] = new ArrayList<>();
             visited[i] = false;
@@ -39,12 +44,20 @@ public class CycleInUndirectedGraph {
         // v 表示parent顶点, u表示当前顶点
         visited[u] = true;
         for (int w : adj[u]) {
+            if (this.hasCycle()) return;
             if (w != v) {
                 // 只往下遍历不往上遍历
                 if (!visited[w]) {
+                    // 当前遍历到的顶点w对应了顶点u
+                    edgeTo[w] = u;
                     dfs(w, u);
                 } else {
-                    hasCycle = true;
+                    cycle = new ArrayDeque<>();
+                    for (int x = u; x != w; x = edgeTo[x]) {
+                        cycle.push(x);
+                    }
+                    cycle.push(w);
+                    cycle.push(u);
                     return;
                 }
             }
@@ -52,7 +65,15 @@ public class CycleInUndirectedGraph {
     }
 
     private boolean hasCycle() {
-        return hasCycle;
+        return cycle != null;
+    }
+
+    private void printCycle() {
+        if (cycle == null) return;
+        while (!cycle.isEmpty()) {
+            System.out.print(cycle.pop() + " ");
+            System.out.println();
+        }
     }
 
     public static void main(String[] args) {
@@ -68,6 +89,7 @@ public class CycleInUndirectedGraph {
 
         if (g.hasCycle()) {
             System.out.println("There is a cycle!");
+            g.printCycle();
         } else {
             System.out.println("There are no cycles!");
         }
